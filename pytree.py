d0ea2a29a53263b19faa09218dfc66b1aa783d63
Ben Krieger <bk494@cornell.edu>
2016-10-05 03:56:25 +0000

    initial commit

--- pytree.py
+++ pytree.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
-
+import os
 
 # YOUR CODE GOES here
 
+def printTree(d, depth) :
+	nbsp = ""
+	depth = depth + 1
+	if (os.path.isdir(d) == False) :
+		for i in range(depth) :
+			nbsp = nbsp + "    "
+		print("|" + nbsp + d)
+	else :
+		print("| -- " + d)
+		dirs = os.listdir(d)
+		for j in dirs :
+			printTree(j, dept
 
 if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+	dirs = os.listdir(os.getcwd())
+	depth = -1
+	for i in dirs: 
+		printTree(i, depth)
+	
+	#print(os.listdir(os.getcwd())[0]);
+	# just for demo
+	# subprocess.run(['tree'] + sys.argv[1:])
