45e651bbe7d7aac5e2c5586df5f4bc17e14c66a5
unknown <bk494@cornell.edu>
2016-10-05 02:59:26 -0400

    unicode

--- pytree.py
+++ pytree.py
@@ -3,57 +3,55 @@ import subprocess
 import sys
 import os
 
-# YOUR CODE GOES here
-
 def printTree(d, depth, fullpath, isLast, pipes) :
 	nbsp = ""
 	depth = depth + 1	
-	#print(fullpath+d)
-	chr = "|"
+	chr = "├──"
 	if isLast :
-		chr = "`"
+		chr = "└──"
 	if (os.path.isdir(fullpath + "/" + d)) :
 		if (depth>0) :
 			for i in range(depth) :
 				try :
 					if pipes.index(i) > -1 : 
-						nbsp = nbsp + "|   "
+						nbsp = nbsp + "│    "
 				except :
-					nbsp = nbsp + "   "
-			nbsp = "|" + nbsp		
-		print(nbsp + chr + "-- " + d)
+					nbsp = nbsp + "    "	
+		print(nbsp + chr + " " + d)
 		dirs = os.listdir(fullpath + "/" + d)
+		dirs.sort()
 		if not isLast and depth > 0:
 			pipes.append(depth)
 		for j in range(len(dirs)) :
 			if not (dirs[j].startswith(".")) :				
 				printTree(dirs[j], depth, fullpath + "/" + d, ( j == len(dirs) - 1), pipes)
 	else :
-		#print(pipes)
 		for i in range(depth) :
 			try :
 				if pipes.index(i) > -1 : 
-					nbsp = nbsp + "|   "
+					nbsp = nbsp + "│    "
 			except :
-				nbsp = nbsp + "   "
+				nbsp = nbsp + "    "
 		if isLast and len(pipes) > 0 :
 			pipes.remove(pipes[len(pipes)-1])
-		if (depth>1) :
-			nbsp = "|" + nbsp
-		print(nbsp + chr + "-- " + d)	
+		print(nbsp + chr + " " + d)	
 
 
 if __name__ == '__main__':
-	cwd = os.getcwd()
+	if len(sys.argv) == 2 :
+		print(sys.argv[1])
+		cwd = sys.argv[1]
+	elif len(sys.argv) == 1 :
+		print(".")
+		cwd = os.getcwd()
+	else :
+		print("incorrect number of arguments")
+		exit()
 	dirs = os.listdir(cwd)
 	depth = -1
 	pipes = []
-	print(".")
+	dirs.sort()
 	for i in range(len(dirs)) : 
 		isLast = ( i == len(dirs) - 1)
 		if not (dirs[i].startswith(".")) :
 			printTree(dirs[i], depth, cwd, isLast, pipes)
-	
-	#print(os.listdir(os.getcwd())[0]);
-	# just for demo
-	# subprocess.run(['tree'] + sys.argv[1:])
