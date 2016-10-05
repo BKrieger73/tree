252bbc0969ca7725a242a3308816577dcf6757ea
unknown <bk494@cornell.edu>
2016-10-05 10:55:20 -0400

    print directory and file counts

--- pytree.py
+++ pytree.py
@@ -3,41 +3,47 @@ import subprocess
 import sys
 import os
 
-def printTree(d, depth, fullpath, isLast, pipes) :
+
+def printTree(d, depth, fullpath, isLast, pipes, directories, files) :
 	nbsp = ""
 	depth = depth + 1	
 	chr = "├──"
 	if isLast :
 		chr = "└──"
 	if (os.path.isdir(fullpath + "/" + d)) :
+		directories = directories + 1
 		if (depth>0) :
 			for i in range(depth) :
 				try :
 					if pipes.index(i) > -1 : 
 						nbsp = nbsp + "│    "
 				except :
-					nbsp = nbsp + "    "	
+					nbsp = nbsp + "    "
 		print(nbsp + chr + " " + d)
 		dirs = os.listdir(fullpath + "/" + d)
 		dirs.sort()
-		if not isLast and depth > 0:
+		if (not isLast ) : 
 			pipes.append(depth)
 		for j in range(len(dirs)) :
 			if not (dirs[j].startswith(".")) :				
-				printTree(dirs[j], depth, fullpath + "/" + d, ( j == len(dirs) - 1), pipes)
+				directories, files = printTree(dirs[j], depth, fullpath + "/" + d, ( j == len(dirs) - 1), pipes, directories, files)
 	else :
+		files = files + 1;
 		for i in range(depth) :
 			try :
 				if pipes.index(i) > -1 : 
 					nbsp = nbsp + "│    "
 			except :
-				nbsp = nbsp + "    "
+				nbsp = nbsp + "    "			
 		if isLast and len(pipes) > 0 :
 			pipes.remove(pipes[len(pipes)-1])
 		print(nbsp + chr + " " + d)	
+	return directories, files
 
 
 if __name__ == '__main__':
+	directories = 0;
+	files = 0;
 	if len(sys.argv) == 2 :
 		print(sys.argv[1])
 		cwd = sys.argv[1]
@@ -54,4 +60,6 @@ if __name__ == '__main__':
 	for i in range(len(dirs)) : 
 		isLast = ( i == len(dirs) - 1)
 		if not (dirs[i].startswith(".")) :
-			printTree(dirs[i], depth, cwd, isLast, pipes)
+			directories, files = printTree(dirs[i], depth, cwd, isLast, pipes, directories, files)
+	print()
+	print(str(directories) + " directories,  " + str(files) + " files")
